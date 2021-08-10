d5fe062723d3e84b45e886b753385f502ab12b7c
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-08-10 19:30:16 +0530

    add sum

---
+++ BZ-KLU1923-PH1-PRACTICE CONTEST - 2021-08-09/Compute area of rectangle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Area_of_rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long length = sc.nextLong();
+        long breadth = sc.nextLong();
+        System.out.println(length * breadth);
+        sc.close();
+    }
+}
