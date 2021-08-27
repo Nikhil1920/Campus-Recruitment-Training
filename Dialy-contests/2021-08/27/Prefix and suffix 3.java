051ac07178414f548c4ed7acc1242f1378b66145
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-08-27 21:37:02 +0530

    add 27-08-2021 daily contest sum

---
+++ Dialy-contests/2021-08/27/Prefix and suffix 3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class PrefixAndSuffix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int s_len = s.length();
+        int count = 0;
+        if (s_len >= 2) {
+            int suf_start = (s_len + 1) / 2;
+            while (suf_start < s_len) {
+                if (s.charAt(suf_start) == s.charAt(count)) {
+                    count++;
+                    suf_start++;
+                } else {
+                    suf_start = suf_start - count + 1;
+                    count = 0;
+                }
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
