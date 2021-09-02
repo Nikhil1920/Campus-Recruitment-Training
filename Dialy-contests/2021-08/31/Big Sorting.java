4943be801853b10d33efb4f12c417ea61ad37f07
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-09-02 16:46:36 +0530

    add 31-09-2021 dialy contest sums

---
+++ Dialy-contests/2021-08/31/Big Sorting.java
@@ -0,0 +1,20 @@
+
+// BigSorting
+import java.util.Arrays;
+import java.util.Scanner;
+
+class BigSorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextLine();
+
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++)
+            System.out.println(arr[i]);
+        sc.close();
+    }
+}
