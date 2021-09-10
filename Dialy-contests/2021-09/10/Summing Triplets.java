11e6d38a47f9ceb6bfc3236707b897144903fade
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-09-10 23:03:31 +0530

    tried string tokenizer and buffer reader

--- Dialy-contests/2021-09/10/Summing Triplets.java
+++ Dialy-contests/2021-09/10/Summing Triplets.java
@@ -1,18 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // Summing Triplets
-import java.util.Scanner;
 
-class Summing {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long num_of_cases = sc.nextLong();
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long num_of_cases = Long.parseLong(reader.readLine());
         while (num_of_cases-- > 0) {
             long sum = 0;
+            StringTokenizer tk = new StringTokenizer(reader.readLine());
             for (int i = 0; i < 3; i++) {
-                sum += sc.nextLong();
+                sum += Long.parseLong(tk.nextToken());
             }
             System.out.println(sum);
         }
-        sc.close();
+        reader.close();
     }
 }
