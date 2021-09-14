2be27ed522eb9edad318099abfccceb8bc92b73e
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-09-14 15:03:01 +0530

    add codeforces Young Physicist 69 a

---
+++ codeforces/problemset/Young Physicist.java
@@ -0,0 +1,19 @@
+// Young Physicist
+// codeforces 69 A
+
+import java.util.Scanner;
+
+class YoungPhysicist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0, y = 0, z = 0;
+        for (int i = 0; i < n; i++) {
+            x += sc.nextInt();
+            y += sc.nextInt();
+            z += sc.nextInt();
+        }
+        System.out.println(x == 0 && y == 0 && z == 0 ? "YES" : "NO");
+        sc.close();
+    }
+}
